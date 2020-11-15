18f977bd86b6a61e01f088c7930fafffafc47cb9
Fedorov Nikita <fedorovni1996@yandex.ru>
2020-11-15 17:01:48 +0500

    3.14 start

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ mainHamburger.addEventListener('click', () => {
   mainMenu.classList.toggle('header__list--active');
   mainHamburger.classList.toggle('hamburger--active');
 })
+
+let footerBtn = document.querySelector('.footer__btn');
+let footerModal = document.querySelector('.modal');
+let footerModalClose = document.querySelector('.modal__close-btn');
+
+footerBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  footerModal.classList.add('modal--active');
+})
+
+footerModal.addEventListener('click', (e) => {
+  if (e.target == footerModal) {
+    footerModal.classList.remove('modal--active');
+  }
+})
+
+footerModalClose.addEventListener('click', () => {
+  footerModal.classList.remove('modal--active');
+})
